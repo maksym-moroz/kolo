51fcc5e26e754b1e19339a158e1d60df2e5b57e0
Maksym Moroz <maksymmoroz@duck.com>
2025-04-11 19:42:49 +0300

    feat(component): start working on reduce implementation

--- shared/src/commonMain/kotlin/com/kolo/component/common/CommonComponent.kt
+++ shared/src/commonMain/kotlin/com/kolo/component/common/CommonComponent.kt
@@ -1,11 +1,18 @@
 package com.kolo.component.common
 
+import com.kolo.action.FeatureAction
 import com.kolo.action.ResultAction
 import com.kolo.component.composition.container.EffectContainer
+import com.kolo.component.composition.context.reduce.ReduceContext
 import com.kolo.component.composition.node.KoloNode
 import com.kolo.state.State
 
-open class CommonComponent<RA : ResultAction, S : State>(
+abstract class CommonComponent<FA : FeatureAction, RA : ResultAction, S : State>(
     override val container: EffectContainer,
     protected val state: S,
-) : KoloNode<RA, S>
+) : KoloNode<RA, S> {
+    abstract fun ReduceContext.processReduce(
+        state: S,
+        action: FA,
+    ): S
+}
--- shared/src/commonMain/kotlin/com/kolo/component/common/KoloComponent.kt
+++ shared/src/commonMain/kotlin/com/kolo/component/common/KoloComponent.kt
@@ -5,17 +5,28 @@ import com.kolo.action.ResultAction
 import com.kolo.component.composition.container.EffectContainer
 import com.kolo.component.composition.context.children.ChildrenContext
 import com.kolo.component.composition.context.children.ChildrenContextDelegate
+import com.kolo.component.composition.context.reduce.ReduceContext
 import com.kolo.state.State
 
 // main component to use on app level
 
-class KoloComponent<FA : FeatureAction, RA : ResultAction, S : State>(
+abstract class KoloComponent<FA : FeatureAction, RA : ResultAction, S : State>(
     effectContainer: EffectContainer,
     initialState: S,
-) : CommonComponent<RA, S>(effectContainer, initialState),
+) : CommonComponent<FA, RA, S>(effectContainer, initialState),
     ChildrenContext<FA, RA, S> by ChildrenContextDelegate(emptySet()) {
-    // ReduceContext
-    // ReduceContext.reduce(state: S, action: FA)
+    final override fun ReduceContext.processReduce(
+        state: S,
+        action: FA,
+    ): S {
+        // todo add needed logic later
+        return reduce(state, action)
+    }
+
+    abstract fun ReduceContext.reduce(
+        state: S,
+        action: FA,
+    ): S
 
     // StoreContext
     // @Composable/SwiftUi
