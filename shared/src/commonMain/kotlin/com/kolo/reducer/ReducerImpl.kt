0cfa6d747d4a9e9a4432048fb33c68146bb945b1
Maksym Moroz <maksymmoroz@duck.com>
2025-04-13 22:27:41 +0300

    bugfix(ios): update swift code to use new composition
    Signed-off-by: Maksym Moroz <maksymmoroz@duck.com>

--- shared/src/commonMain/kotlin/com/kolo/reducer/ReducerImpl.kt
+++ shared/src/commonMain/kotlin/com/kolo/reducer/ReducerImpl.kt
@@ -1,12 +1,13 @@
 package com.kolo.reducer
 
 import com.kolo.action.Action
+import com.kolo.action.ResultAction
 import com.kolo.component.common.KoloComponent
 import com.kolo.component.composition.context.reduce.ReduceContext
 import com.kolo.state.State
 
 class ReducerImpl<S : State>(
-    private val component: KoloComponent<*, S>,
+    private val component: KoloComponent<out ResultAction, S>,
     private val context: ReduceContext,
 ) : Reducer<S> {
     override fun reduce(
